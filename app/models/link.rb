02d15ae1c458c372e60b775053fb1f9f59bb5054
Shi Hao <shihaotan91@gmail.com>
2022-06-20 22:42:34 +0800

    add length validation to short_url

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,7 +4,7 @@ class Link < ApplicationRecord
   before_validation :process_link, on: [:create]
 
   validates :long_url, :short_url, :salt, presence: true
-  validates :short_url, uniqueness: true
+  validates :short_url, uniqueness: true, length: { is: 6 }
 
   def process_link
     ProcessLink.new(self).call
